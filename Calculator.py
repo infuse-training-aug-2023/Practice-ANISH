f4b0e27c184e17b555d87390be5a719190347466
ANish Naik <naikanish13@gmail.com>
2023-08-17 12:27:15 +0530

    sum adding fun done

---
+++ Calculator.py
@@ -0,0 +1,46 @@
+class Calc:
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+
+
+    def get_input(self):
+        self.a = int(input("Enter the first Number : "))
+        self.b = int(input("Enter the Second Number : "))
+        operation = input("What You wna t to Do?(+,-,*,/):")
+        
+        if operation == "+":
+            self.sum()
+        
+        else :
+            print("Wrong Operation!!!!")
+
+    def print(self,operation,sum):
+        print(str(self.a) + " "  + operation + " " + str(self.b) +" = "+str(sum))
+    
+    def sum():
+        sum = self.a +self.b
+        self.print(operation,sum)
+    
+
+
+
+c = Calc()
+
+c.get_input()
+
+# elif operation == "-":
+#             self.sum()
+#         elif operation == "/":
+#             self.sub()
+#         elif operation == "*":
+#             self.mul()
+# def sub():
+    #     sum = self.a -self.b
+    #     self.print(operation,sum)
+    # def mul():
+    #     sum = self.a *self.b
+    #     self.print(operation,sum)
+    # def div():
+    #     sum = self.a /self.b
+    #     self.print(operation,sum)
