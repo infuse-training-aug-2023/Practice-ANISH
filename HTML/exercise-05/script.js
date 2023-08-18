3349ad885ce06736ff6d7e2ecbd43cb1902f4fb1
ANish Naik <naikanish13@gmail.com>
2023-08-18 13:11:50 +0530

    added responsive ness to tiles exe 5

--- HTML/exercise-05/script.js
+++ HTML/exercise-05/script.js
@@ -6,19 +6,32 @@ var found = false
 var textinput = document.getElementById("input")
 var btn = document.getElementById("Search")
 var cardCont = document.getElementsByClassName("cardCont")[0]
+var paginationDiv = document.getElementById("paginationDiv");
 
+paginationDiv.style.display = "none";
+var currettext = ""
+var page = 1
 
 
 
 btn.onclick = getData
 
 function getData(){
+    paginationDiv.style.display = "none";
     fetch("http://www.omdbapi.com/?apikey=fd717b92&type=movie&s="+textinput.value).then(res => {
         res.json().then(r => {
             if (r.Response === "True"){
+                // console.log(r,);
                 movies = r.Search
                 console.log(r);
                 found = true
+                if (parseInt(r.totalResults) > 10){
+                    paginationDiv.style.display = "flex";
+                    currettext = textinput.value
+                }
+                while (cardCont.firstChild) {
+                  cardCont.removeChild(cardCont.firstChild);
+                }
                 movies.forEach(element => {
                     createCard(element)
                 });
@@ -43,25 +56,26 @@ function createCard(data){
     imgcont.setAttribute("class", "imgCont")
     var imgnode = document.createElement("img")
     imgnode.setAttribute("src", data.Poster)
+    imgnode.setAttribute("alt",data.Title)
     imgcont.appendChild(imgnode)
 
     //creating title div
     var titlediv = document.createElement("div")
     titlediv.setAttribute("class", "desc")
     var titlep = document.createElement("p")
-    titlep.innerText = "Title : " + data.Title
+    titlep.innerText = "Title : " + (data.Title.length > 20 ? data.Title.slice(0,20)+ "..." : data.Title)
     titlediv.appendChild(titlep)
 
     //creating year div
-    var yeardiv = document.createElement("div")
-    yeardiv.setAttribute("class", "desc")
+    // var yeardiv = document.createElement("div")
+    // yeardiv.setAttribute("class", "desc")
     var yearp = document.createElement("p")
-    yearp.innerText = "Year Of Release : " + data.Year
-    yeardiv.appendChild(yearp)
+    yearp.innerText = "Year : " + data.Year
+    titlediv.appendChild(yearp)
 
     ele.appendChild(imgcont)
     ele.appendChild(titlediv)
-    ele.appendChild(yeardiv)
+    // ele.appendChild(yeardiv)
 
     cardCont.appendChild(ele)
 }
