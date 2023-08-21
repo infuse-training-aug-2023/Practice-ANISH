cfe28fd8380650dca17e96a9c7c5accc3ec72b47
ANish Naik <naikanish13@gmail.com>
2023-08-21 12:02:08 +0530

    added dark mode to 3rd

--- HTML/exercise-02/script.js
+++ HTML/exercise-02/script.js
@@ -1,8 +1,23 @@
 var clock = document.getElementById("clock")
-
+var toggle  = false
 
 function tick(){
     clock.innerText = new Date().toLocaleTimeString()
 }
 
+
+
+var toggleBtn = document.getElementById("Toogle")
+var container = document.getElementsByClassName("container")[0]
 setInterval(tick, 1000);
+
+toggleBtn.onclick = ()=>{
+    if(toggle){
+        container.className = container.className +" dark"
+        
+    }
+    else{
+        container.className = container.classList[0]
+    }
+    toggle = !toggle
+}
