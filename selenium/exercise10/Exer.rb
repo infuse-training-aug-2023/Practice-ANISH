0c570c15d6fa8202a20f18a954a5457a73102eba
ANish Naik <naikanish13@gmail.com>
2023-08-22 13:04:27 +0530

    fixing

--- selenium/exercise10/Exer.rb
+++ selenium/exercise10/Exer.rb
@@ -2,4 +2,13 @@ require "selenium-webdriver"
 
 Selenium::WebDriver::Chrome::Service.driver_path = "C:/Users/naika/OneDrive/Documents/Drivers/chromedriver.exe"
 
-$driver = Selenium::WebDriver.for :chrome
+driver = Selenium::WebDriver.for :chrome
+
+# Column nomber
+col = 2
+
+driver.get "https://computer-database.gatling.io/computers"
+
+table = driver.find_element(:tag_name, "table")
+
+driver.quit
